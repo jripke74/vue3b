b165bebc24f46bd6cb140b128e159e0a8a617906
Jeff Ripke <jripke74@gmail.com>
2021-09-01 10:44:52 -0700

    06 add courseGoalA and courseGoalB data and this. to outputGoal()

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const app = Vue.createApp( {
   data() {
     return {
-      courseGoal: 'Complete the Vue3 course and master Vue3!',
+      courseGoalA: 'Complete the Vue3 course!',
+      courseGoalB: 'Master Vue3 and build amazing apps!',
       vueLink: 'https://vuejs.org',
     };
   },
@@ -9,9 +10,9 @@ const app = Vue.createApp( {
     outputGoal() {
       const randomNumber = Math.random();
       if (randomNumber < 0.5) {
-        return 'Learn Vue!';
+        return this.courseGoalA;
       } else {
-        return 'Master Vue';
+        return this.courseGoalB;
       }
     },
   },
